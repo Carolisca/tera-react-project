e2463ec7935e2ee934e302bb9d31ddf0fe0ea431
Carolisca <carol.carolisca@gmail.com>
2023-06-18 16:52:27 -0300

    Adiciona Roteamento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-//import Home from "./components/pages/Home";
-//import Users from "./components/pages/Users";
-//import UserBlog from "./components/pages/UserBlog";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Users from "./components/pages/Users";
+import UserBlog from "./components/pages/UserBlog";
 import UserPostForm from "./components/pages/UserPostForm";
 
 import "./styles/normalize.css";
@@ -9,7 +12,15 @@ import "./styles/main.css";
 
 
 function App() {
-  return <UserPostForm />;
+  return (
+  <BrowserRouter>
+  <Routes>
+<Route path="/" element={<Home />}/>
+<Route path= "/users" element={<Users />}/>
+<Route path= "/users/:userId" element={<UserBlog />}/>
+<Route path= "/users/:userId/post" element={<UserPostForm />}/>
+  </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
